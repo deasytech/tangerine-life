192ae9302f9e1112d20404efb5a7342a3b1c2b58
Desmond Awere <desmondawere@gmail.com>
2024-10-02 14:52:29 +0100

    contact tab updated

--- components/ContactTabs.tsx
+++ components/ContactTabs.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { ArrowDown, ArrowRight } from 'lucide-react';
 import React, { useState } from 'react';
@@ -7,92 +7,62 @@ interface Tab {
   key: string;
   label: string;
   content: {
-    location: React.ReactNode,
-    address: React.ReactNode,
-  };
+    location: string;
+    address: string;
+  }[];
 }
 
 const tabs: Tab[] = [
   {
-    key: 'general', label: 'Tangerine General', content: {
-      location: <div>
-        <p>Lagos Head office - Yaba</p>
-        <p>Osogbo</p>
-        <p>Retail Business OUTLET KANO</p>
-      </div>,
-      address: <div>
-        <p>14, Hughes Avenue, Alagomeji, Yaba, Lagos</p>
-        <p>2nd Floor, Jesus Court, Isiaka Adeleke Freeway, Okefia, Oshogbo, Osun state.</p>
-        <p>Skye Bank Plc, 23, Bello Road, Kano, Kano state</p>
-      </div>
-    }
+    key: 'general',
+    label: 'Tangerine General',
+    content: [
+      { location: 'Lagos Head office - Yaba', address: '14, Hughes Avenue, Alagomeji, Yaba, Lagos' },
+      { location: 'Osogbo', address: '2nd Floor, Jesus Court, Isiaka Adeleke Freeway, Okefia, Oshogbo, Osun state.' },
+      { location: 'Retail Business OUTLET KANO', address: 'Skye Bank Plc, 23, Bello Road, Kano, Kano state' },
+    ]
   },
   {
-    key: 'life', label: 'Tangerine Life', content: {
-      location: <div>
-        <p>Abuja</p>
-        <p>Benin</p>
-        <p>Ibadan</p>
-        <p>Kaduna</p>
-        <p>Retail Business OUTLET Abeokuta</p>
-        <p>Retail Business OUTLET AKURE</p>
-        <p>Retail Business OUTLET ONITSHA</p>
-      </div>,
-      address: <div>
-        <p>5, Dambata close, Area 7, Garki, FCT</p>
-        <p>1st Floor of Austin Lying Plaza, 99A, First East Circular Road, Opposite Uvbi Primary School, By St Joseph Catholic Church, Benin City.</p>
-        <p>1st Floor, Heritage Mall, Cocoa House, Dugbe, Ibadan</p>
-        <p>Hasfsat Plaza, 4 Constitution Road Kaduna</p>
-        <p>23C Segun Osoba Way, Abeokuta</p>
-        <p>BOI building, Alagbaka, Akure</p>
-        <p>65 New Market Road, Onitsha</p>
-      </div>
-    }
+    key: 'life',
+    label: 'Tangerine Life',
+    content: [
+      { location: 'Abuja', address: '5, Dambata close, Area 7, Garki, FCT' },
+      { location: 'Benin', address: '1st Floor of Austin Lying Plaza, 99A, First East Circular Road, Opposite Uvbi Primary School, By St Joseph Catholic Church, Benin City.' },
+      { location: 'Ibadan', address: '1st Floor, Heritage Mall, Cocoa House, Dugbe, Ibadan' },
+      { location: 'Kaduna', address: 'Hasfsat Plaza, 4 Constitution Road Kaduna' },
+      { location: 'Retail Business OUTLET Abeokuta', address: '23C Segun Osoba Way, Abeokuta' },
+      { location: 'Retail Business OUTLET AKURE', address: 'BOI building, Alagbaka, Akure' },
+      { location: 'Retail Business OUTLET ONITSHA', address: '65 New Market Road, Onitsha' },
+    ]
   },
   {
-    key: 'pensions', label: 'Tangerine Pensions', content: {
-      location: <div>
-        <p>Head office (Main Office)</p>
-        <p>Lagos</p>
-        <p>Kano Office</p>
-        <p>Kaduna Office</p>
-        <p>Gombe Office</p>
-      </div>,
-      address: <div>
-        <p>FMBN House, Plot 266, CBD, Abuja</p>
-        <p>21A & 21B Olosa Street, Victoria Island, Lagos</p>
-        <p>7 Zoo Road, Prime Hub Shopping Complex, Kano</p>
-        <p>BOI Building, 18 Mohammadu Buhari Way, Kaduna</p>
-        <p>CFA Building, Jeka da Fari, Gombe</p>
-      </div>
-    }
+    key: 'pensions',
+    label: 'Tangerine Pensions',
+    content: [
+      { location: 'Head office (Main Office)', address: 'FMBN House, Plot 266, CBD, Abuja' },
+      { location: 'Lagos', address: '21A & 21B Olosa Street, Victoria Island, Lagos' },
+      { location: 'Kano Office', address: '7 Zoo Road, Prime Hub Shopping Complex, Kano' },
+      { location: 'Kaduna Office', address: 'BOI Building, 18 Mohammadu Buhari Way, Kaduna' },
+      { location: 'Gombe Office', address: 'CFA Building, Jeka da Fari, Gombe' },
+    ]
   },
   {
-    key: 'health', label: 'Tangerine Health', content: {
-      location: <div>
-        <p>Lagos office - Palmgrove Estate</p>
-        <p>Abuja</p>
-        <p>Ibadan</p>
-        <p>Enugu</p>
-        <p>Yola</p>
-        <p>Ilorin</p>
-        <p>Port Harcourt</p>
-      </div>,
-      address: <div>
-        <p>2 Marconi Road, Palmgrove Estate, Lagos</p>
-        <p>1129 Zakariya Malami Street, Abuja</p>
-        <p>14th Floor, Cocoa House, Ibadan</p>
-        <p>16 Marcus Harvey Street, Enugu</p>
-        <p>Bekaji-Jimeta, Yola</p>
-        <p>16B Cocacola Road, Ilorin</p>
-        <p>26A Aba Road, Port Harcourt</p>
-      </div>
-    }
-  },
+    key: 'health',
+    label: 'Tangerine Health',
+    content: [
+      { location: 'Lagos office - Palmgrove Estate', address: '2 Marconi Road, Palmgrove Estate, Lagos' },
+      { location: 'Abuja', address: '1129 Zakariya Malami Street, Abuja' },
+      { location: 'Ibadan', address: '14th Floor, Cocoa House, Ibadan' },
+      { location: 'Enugu', address: '16 Marcus Harvey Street, Enugu' },
+      { location: 'Yola', address: 'Bekaji-Jimeta, Yola' },
+      { location: 'Ilorin', address: '16B Cocacola Road, Ilorin' },
+      { location: 'Port Harcourt', address: '26A Aba Road, Port Harcourt' },
+    ]
+  }
 ];
 
 const ContactTabs: React.FC = () => {
-  const [ activeTab, setActiveTab ] = useState<string>(tabs[ 1 ].key);
+  const [ activeTab, setActiveTab ] = useState<string>(tabs[ 0 ].key);
 
   const handleTabClick = (key: string) => {
     setActiveTab(key);
@@ -131,10 +101,16 @@ const ContactTabs: React.FC = () => {
                   </tr>
                 </thead>
                 <tbody>
-                  <tr>
-                    <td className="pr-8 align-top paragraph">{tab.content.location}</td>
-                    <td className="align-top paragraph">{tab.content.address}</td>
-                  </tr>
+                  {tab.content.map((item, index) => (
+                    <tr key={index} className="mb-4">
+                      <td className="pr-8 py-4 align-top paragraph border-b">
+                        {item.location}
+                      </td>
+                      <td className="py-4 align-top paragraph border-b">
+                        {item.address}
+                      </td>
+                    </tr>
+                  ))}
                 </tbody>
               </table>
             </div>
